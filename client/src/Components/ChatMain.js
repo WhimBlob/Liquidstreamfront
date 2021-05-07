d973e1dbe57f4b379fb8c6ca00e20b91220a40b4
Ughetto Jules <ughetto.jules@gmail.com>
2021-05-07 23:57:32 +0200

    End

---
+++ client/src/Components/ChatMain.js
@@ -0,0 +1,8 @@
+{/* <div className="chat-main">
+<div className="chat-sidebar">
+  <h3><i className="fas fa-comments"></i>Chat</h3>
+  <h3><i className="fas fa-users"></i> Users</h3>
+  <ul id="users"></ul>
+</div>
+<div className="chat-messages"></div>
+</div> */}
---
+++ client/src/Components/Header.js
@@ -0,0 +1,27 @@
+// import React from 'react';
+// import Registration from './Registration';
+// import Login from './Login';
+// import Logout from './Logout';
+// var user = localStorage.getItem('username')
+
+// function Header() {
+//   if (!user) {
+//     return(
+//       <header className="chat-header">
+//         <h1><i className="fas fa-smile"></i>LiquidStream</h1>
+//         <Registration />
+//         <Login />
+//       </header>
+//     )
+//   }
+//   else {
+//     return(
+//       <header className="chat-header">
+//         <h1>{user}</h1>
+//         <Logout />
+//       </header>
+//     )
+//   }
+// }
+
+// export default Header;
